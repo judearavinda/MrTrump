4b3e90977c72b98e2da27156119e6737fd77600a
Tejpal <tssahota@uwaterloo.ca>
2016-11-26 18:32:35 -0500

    Deez Nuts
    Kuck'd and given up

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -3,6 +3,10 @@ import { Template } from 'meteor/templating';
 import './body.html';
 
 Template.body.events({
-
+'click .mainButton'(event){
+  console.log("kappa");
+  var randommsg = "Suck deez nuts;"
+  $(".msg").text(randommsg);
+}
 
 });
