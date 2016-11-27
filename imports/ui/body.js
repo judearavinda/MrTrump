a71dea3be826724651fe91eb11044927df5c4a10
Jude Tillekeratne <judecanada911@hotmail.com>
2016-11-26 19:27:10 -0500

    Added code to generate random tweets

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,35 @@
 import { Template } from 'meteor/templating';
 
 import './body.html';
-
+var randomnumber1 = -1;
+var array = ["Fidel Castro is dead!","Happy Thanksgiving to everyone. We will, together, MAKE AMERICA GREAT AGAIN!",
+"If Obama resigns from office NOW, thereby doing a great service to the country—I will give him free lifetime golf at any one of my courses!",
+"Obama is, without question, the WORST EVER president. I predict he will now do something really bad and totally stupid to show manhood!",
+"This election is a total sham and a travesty. We are not a democracy!",
+"I have never seen a thin person drinking Diet Coke.",
+"Robert Pattinson should not take back Kristen Stewart. She cheated on him like a dog & will do it again--just watch. He can do much better!",
+"Robert I'm getting a lot of heat for saying you should dump Kristen- but I'm right. If you saw the Miss Universe girls you would reconsider.",
+"So many tweets & stories on Stewart/Pattinson Look, it doesn't matter-- the relationship will never be the same. It is permanently broken.",
+"Everyone knows I am right that Robert Pattinson should dump Kristen Stewart. In a couple of years, he will thank me. Be smart, Robert.",
+"After Friday’s Twilight release, I hope Robert Pattinson will not be seen in public with Kristen--she will cheat on him again!",
+"@katyperry Katy, what the hell were you thinking when you married loser Russell Brand. There is a guy who has got nothing going, a waste!",
+"@katyperry must have been drunk when she married Russell Brand @rustyrockets – but he did send me a really nice letter of apology!",
+"@katyperry is no bargain but I don't like John Mayer--he dates and tells--be careful Katy (just watch!).",
+"@ariannahuff is unattractive both inside and out. I fully understand why her former husband left her for a man- he made a good decision.",
+"Pervert alert. @RepWeiner is back on twitter. All girls under the age of 18, block him immediately."
+];
 Template.body.events({
 'click .mainButton'(event){
   console.log("kappa");
-  var randommsg = "Suck deez nuts";
+  var maximum = 15;
+  var minimum = 0;
+  var imageMaximum = 0;
+  var imageMinimum = 4;
+  var randomnumber2 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+  var imageRandom = Math.floor(Math.random() * (imageMaximum - imageMinimum + 1)) + imageMinimum;
+  randomnumber1 = randomnumber2;
+
+  var randommsg = array[randomnumber2];
   $(".msg").text(randommsg);
 }
 
